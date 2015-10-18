e69fd41d64335cc292279844f6cb322833267629
Heng-xiu <r23456999@gmail.com>
2015-10-18 23:51:02 +0800

    init views & index.html

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+var path = require('path');
+
+// Set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('hogan-express'));
+app.set('view engine', 'html'); // find index.html in views folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Route
 app.route('/').get(function( req, res, next){
-	res.send('<h1>Hello world</h1>');
+	res.render('index', {});
 });
-
 
 app.listen(port, function() {
 	console.log('ChaChaT is working on Port 3000');
