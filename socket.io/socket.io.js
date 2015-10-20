45672702a9dc616b69a2c4f922058926e6955499
Heng-xiu <r23456999@gmail.com>
2015-10-20 14:28:34 +0800

    確認房間reflash不會消失

--- socket.io/socket.io.js
+++ socket.io/socket.io.js
@@ -1,7 +1,9 @@
 module.exports = function(io, rooms) {
 	var chatrooms = io.of('/roomlist').on('connection', function(socket) {
 		console.log('[socket.js]connection establish on server !');
-
+		// importmant
+		socket.emit('roomupdate', JSON.stringify(rooms));
+		// 監聽是否有房間更新
 		socket.on('newroom', function(data){
 			console.log('data:' + data);
 			rooms.push(data);
