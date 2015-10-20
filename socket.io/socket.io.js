cdea37e3fda05b3b39c4bd5da1e48045286ee380
Heng-xiu <r23456999@gmail.com>
2015-10-20 14:26:23 +0800

    create char roomS

--- socket.io/socket.io.js
+++ socket.io/socket.io.js
@@ -1,5 +1,12 @@
 module.exports = function(io, rooms) {
 	var chatrooms = io.of('/roomlist').on('connection', function(socket) {
 		console.log('[socket.js]connection establish on server !');
+
+		socket.on('newroom', function(data){
+			console.log('data:' + data);
+			rooms.push(data);
+			socket.broadcast.emit('roomupdate', JSON.stringify(rooms));
+			socket.emit('roomupdate', JSON.stringify(rooms));
+		});
 	});
 };
