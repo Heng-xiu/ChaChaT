e8c651a0e255621b6641ecade7e0da977428b78e
Heng-xiu <r23456999@gmail.com>
2015-10-21 21:05:24 +0800

    部署前檢查 && BUG in rooms

--- socket.io/socket.io.js
+++ socket.io/socket.io.js
@@ -17,10 +17,38 @@ module.exports = function(io, rooms) {
 			socket.userName = data.userName;
 			socket.userPic = data.userPic;
 			socket.join(data.roomNum);
+			updateUserList(data.roomNum, true);
 		});
 
 		socket.on('newMessage', function(data){
 			socket.broadcast.to(data.room_number).emit('messagefeed', JSON.stringify(data));
+		});
+
+		function updateUserList(roomNum, updateAll) {
+			// BUG 目前socket.io 沒法抓該room的client
+			var roomsocketId = io.nsps['/messages'].adapter.rooms[roomNum];
+			roomsocketId = Object.keys(roomsocketId)[0];
+			var getUsers = io.nsps['/messages'].connected[roomsocketId];
+			var userList = [];
+			// for(var i in getUsers){
+			// 	userList.push({
+			// 		// user: getUsers[i].userName,
+			// 		// userPic: getUsers[i].userPic,
+			// 	});
+			// }
+			userList.push({
+				user: getUsers.userName,
+				userPic: getUsers.userPic,
+			});
+			socket.emit('updateUsersList', JSON.stringify(userList)).to(roomNum);
+
+			if(updateAll){
+				socket.broadcast.to(roomNum).emit('updateUsersList', JSON.stringify(userList));
+			}
+		}
+
+		socket.on('updateList', function(data){
+			updateUserList(data.room);
 		});
 	});
 };
